70e87f632ce38b5fff62c0a2c2bf4eee1d1de3f2
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-05-04 09:45:20 +0600

    small fix-allignment

--- app/(main)/personnel/page.tsx
+++ app/(main)/personnel/page.tsx
@@ -3,13 +3,14 @@ import PersonnelList from "@/app/ui/main/personnel/personnel-list";
 
 export default function Page() {
   return (
-  <div className="w-full relative">
-  <div className="flex w-full items-center justify-between">
-    <h1 className="text-2xl">Personnel List</h1>
-  </div>
-  <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-    <AddNewPersonnel />    
-  </div>
-  <PersonnelList />
-</div>);
+    <div className="w-full relative">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl">Personnel List</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <AddNewPersonnel />
+      </div>
+      <PersonnelList />
+    </div>
+  );
 }
