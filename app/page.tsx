610d5476cb0b6a8fe3e1acc59bab9029b2f102e9
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-05-25 14:55:20 +0600

    added signin page and link to signin page into the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <h1>Project starts</h1>;
+import Link from "next/link";
+
+export default async function Home() {
+  return (
+    <div className="w-full h-screen text-center flex flex-col justify-center gap-10">
+      <h1 className="text-5xl font-bold">
+        <span className=" bg-clip-text text-transparent bg-gradient-to-tl from-blue-600 to-green-500 drop-shadow-lg">
+          Welcome,
+        </span>
+        <br />
+        <span className="text-slate-600 drop-shadow">to</span>
+        <br />
+        <span className=" bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-orange-300 to-green-300">
+          Competency Assessment Questionniare
+        </span>
+      </h1>
+      <p className="font-medium">
+        To go further, please{" "}
+        <Link href="/signin" className="text-blue-600 underline">
+          sign in
+        </Link>
+      </p>
+    </div>
+  );
 }
